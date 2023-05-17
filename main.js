c6d5e8a124edb584f65e00227ad6bc6a344fd96c
Nathalia Garcia Espinel <119705526+NathEspinel@users.noreply.github.com>
2023-05-16 19:41:04 -0500

    Responsive

---
+++ main.js
@@ -0,0 +1,9 @@
+/*let botonSuzume = document.getElementById('suzume')
+botonSuzume.addEventListener('click', link)
+
+let botonTenki = document.getElementById('tenki')
+botonTenki.addEventListener('click', link)
+
+let botonKimi = document.getElementById('kimi')
+botonKimi.addEventListener('click', link)*/
+
